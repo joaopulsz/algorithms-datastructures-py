2c1793ce484946cc08cb8b4fa3fd88377aa8009c
joaopulsz <joaopulsz@hotmail.com>
2022-11-01 21:46:20 +0000

    added pop method to linked list

--- linked_list.py
+++ linked_list.py
@@ -17,7 +17,7 @@ class LinkedList:
             print(i.value)
             i = i.next
 
-    def append(self, value):
+    def append(self, value):  # adds an item to the end o the list
         new_node = Node(value)
         if self.tail:
             self.tail.next = new_node
@@ -26,8 +26,27 @@ class LinkedList:
         self.tail = new_node
         self.length += 1
 
+    def pop(self):  # removes and returns the item at the tail of the list
+        if self.length == 0:
+            return None
+        item = self.head
+        pre = self.head
+        while item.next:
+            pre = item
+            item = item.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return item
+
 
 linked_list = LinkedList(1)
-linked_list.append(2)
-linked_list.append(5)
+linked_list.append(3)
+linked_list.append(4)
+
+linked_list.pop()
+
 linked_list.print_list()
