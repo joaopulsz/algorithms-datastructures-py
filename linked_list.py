8276a741e3e3a851a731c260259a23632fb56b4f
joaopulsz <joaopulsz@hotmail.com>
2022-11-01 20:53:30 +0000

    added append method to linked list

--- linked_list.py
+++ linked_list.py
@@ -16,3 +16,18 @@ class LinkedList:
         while i is not None:
             print(i.value)
             i = i.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.tail:
+            self.tail.next = new_node
+        else:
+            self.head = new_node
+        self.tail = new_node
+        self.length += 1
+
+
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(5)
+linked_list.print_list()
