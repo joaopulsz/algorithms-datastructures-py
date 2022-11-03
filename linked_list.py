f5004f11e39ef7bea61a295076987ea2da260914
joaopulsz <joaopulsz@hotmail.com>
2022-11-03 17:13:32 +0000

    added insert method

--- linked_list.py
+++ linked_list.py
@@ -17,7 +17,7 @@ class LinkedList:
             print(i.value)
             i = i.next
 
-    def append(self, value):  # adds an item to the end o the list
+    def append(self, value):  # adds a node to the end o the list
         new_node = Node(value)
         if self.tail:
             self.tail.next = new_node
@@ -25,8 +25,9 @@ class LinkedList:
             self.head = new_node
         self.tail = new_node
         self.length += 1
+        return True
 
-    def pop(self):  # removes and returns the item at the tail of the list
+    def pop(self):  # removes and returns the node at the tail of the list
         if self.length == 0:
             return None
         item = self.head
@@ -42,7 +43,7 @@ class LinkedList:
             self.tail = None
         return item
 
-    def prepend(self, value):  # adds value to the start of the list
+    def prepend(self, value):  # adds a new node to the start of the list
         new_node = Node(value)
         if self.length != 0:
             new_node.next = self.head
@@ -50,8 +51,9 @@ class LinkedList:
             self.tail = new_node
         self.head = new_node
         self.length += 1
+        return True
 
-    def pop_first(self):  # removes and returns the item at the head of the list
+    def pop_first(self):  # removes and returns the node at the head of the list
         if self.length == 0:
             return None
         item = self.head
@@ -75,13 +77,28 @@ class LinkedList:
         item = self.get(index)
         if item:
             item.value = value
+            return True
+        return False
+
+    def insert(self, index, value):  # inserts a new node at the given index position
+        new_node = Node(value)
+        if index > self.length or index < 0:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        else:
+            pre = self.get(index - 1)
+            new_node.next = pre.next
+            pre.next = new_node
+        self.length += 1
+        return True
 
 
 linked_list = LinkedList(1)
 linked_list.append(4)
-linked_list.append(5)
-linked_list.prepend(8)
 
-linked_list.set_value(0, 3)
+linked_list.insert(1, 3)
 
 linked_list.print_list()
