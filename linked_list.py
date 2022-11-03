e1ba9eb0ca297ed7e7aa9f50ced83c110975f568
joaopulsz <joaopulsz@hotmail.com>
2022-11-03 18:08:17 +0000

    added reverse method to linked list

--- linked_list.py
+++ linked_list.py
@@ -109,12 +109,24 @@ class LinkedList:
         self.length -= 1
         return item
 
+    def reverse(self):  # reverses the order of the nodes in the list
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        post = temp.next
+        pre = None
+        for _ in range(self.length):
+            post = temp.next
+            temp.next = pre
+            pre = temp
+            temp = post
+
 
 linked_list = LinkedList(1)
 linked_list.append(4)
 linked_list.append(2)
 linked_list.append(8)
 
-linked_list.remove(2)
+linked_list.reverse()
 
 linked_list.print_list()
