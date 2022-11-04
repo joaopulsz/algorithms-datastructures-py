b3ef210c387e4a56531c82039df61a83ab35eaa2
joaopulsz <joaopulsz@hotmail.com>
2022-11-04 07:53:01 +0000

    added constructors and print method to doubly linked list

---
+++ doubly_linked_list.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.previous = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        i = self.head
+        while i is not None:
+            print(i.value)
+            i = i.next
+
+
+doubly_linked_list = DoublyLinkedList(5)
+
+doubly_linked_list.print_list()
