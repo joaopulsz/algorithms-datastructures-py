25145ebf752ad1a84613e8e9805bf2fb023c90b6
joaopulsz <joaopulsz@hotmail.com>
2022-11-06 18:15:35 +0000

    added enqueue method

--- queue.py
+++ queue.py
@@ -16,3 +16,20 @@ class Queue:
         while i is not None:
             print(i.value)
             i = i.next
+
+    def enqueue(self, value):  # adds a new node to the end of the queue
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+
+queue = Queue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+queue.print_queue()
