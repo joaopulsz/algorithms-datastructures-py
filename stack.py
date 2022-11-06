27309bc0bde2f3d16264014d080dd6f371d10cdb
joaopulsz <joaopulsz@hotmail.com>
2022-11-06 11:43:34 +0000

    added push method to stack

--- stack.py
+++ stack.py
@@ -15,3 +15,17 @@ class Stack:
         while i is not None:
             print(i.value)
             i = i.next
+
+    def push(self, value):  # adds a new node to the top of the stack
+        new_node = Node(value)
+        if self.height != 0:
+            new_node.next = self.top
+        self.top = new_node
+        self.height += 1
+
+
+stack = Stack(1)
+stack.push(3)
+stack.push(2)
+
+stack.print_stack()
