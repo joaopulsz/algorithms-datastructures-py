7e81a23df405e9825bc362eb58803be3b3e3c198
joaopulsz <joaopulsz@hotmail.com>
2022-11-06 12:11:19 +0000

    added queue constructors and print method

---
+++ queue.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        i = self.first
+        while i is not None:
+            print(i.value)
+            i = i.next
