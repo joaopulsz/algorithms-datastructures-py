5013bd3acfa1b7c9b6d0d1e6f4893d45af7376e8
joaopulsz <joaopulsz@hotmail.com>
2022-11-08 13:57:24 +0000

    added set method to HT

--- hash_table.py
+++ hash_table.py
@@ -12,7 +12,17 @@ class HashTable:
         for key, value in enumerate(self.data_map):
             print(key, ": ", value)
 
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
 
 hash_table = HashTable()
+
+hash_table.set_item('guitars', 3)
+hash_table.set_item('pianos', 1)
+hash_table.set_item('flutes', 6)
 
 hash_table.print_table()
