5ddbcf5b6fe74d7732efc2c45f4774a1094b9ee2
joaopulsz <joaopulsz@hotmail.com>
2022-11-09 16:19:19 +0000

    added remove edge method

--- graph.py
+++ graph.py
@@ -13,10 +13,17 @@ class Graph:
         return False
 
     def add_edge(self, vertex1, vertex2):
-        if vertex1 not in self.adj_list or vertex2 not in self.adj_list:
+        if vertex1 not in self.adj_list.keys() or vertex2 not in self.adj_list.keys():
             return False
         self.adj_list[vertex1].append(vertex2)
         self.adj_list[vertex2].append(vertex1)
+        return True
+
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 not in self.adj_list.keys() or vertex2 not in self.adj_list.keys() or vertex1 not in self.adj_list[vertex2] or vertex2 not in self.adj_list[vertex1]:
+            return False
+        self.adj_list[vertex1].remove(vertex2)
+        self.adj_list[vertex2].remove(vertex1)
         return True
 
 
@@ -24,8 +31,16 @@ graph = Graph()
 
 graph.add_vertex("J")
 graph.add_vertex("O")
+graph.add_vertex("A")
+graph.add_vertex("P")
 
-print(graph.add_edge("J", "A"))
 print(graph.add_edge("O", "J"))
+print(graph.add_edge("O", "A"))
+print(graph.add_edge("A", "J"))
+
+graph.print_graph()
+
+print(graph.remove_edge("O", "J"))
+print(graph.remove_edge("J", "P"))
 
 graph.print_graph()
