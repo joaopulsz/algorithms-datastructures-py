04d781726be570a07271e3551c4041ad861c8b8e
joaopulsz <joaopulsz@hotmail.com>
2022-11-09 16:04:01 +0000

    added add edge method to graph

--- graph.py
+++ graph.py
@@ -12,10 +12,20 @@ class Graph:
             return True
         return False
 
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 not in self.adj_list or vertex2 not in self.adj_list:
+            return False
+        self.adj_list[vertex1].append(vertex2)
+        self.adj_list[vertex2].append(vertex1)
+        return True
+
 
 graph = Graph()
 
 graph.add_vertex("J")
 graph.add_vertex("O")
+
+print(graph.add_edge("J", "A"))
+print(graph.add_edge("O", "J"))
 
 graph.print_graph()
