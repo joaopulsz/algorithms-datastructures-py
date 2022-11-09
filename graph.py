71f0959d167d3faca1eb5f1cd28c28ca33714a68
joaopulsz <joaopulsz@hotmail.com>
2022-11-09 15:57:58 +0000

    added print and add vertex methods

--- graph.py
+++ graph.py
@@ -1,3 +1,21 @@
 class Graph:
     def __init__(self):
         self.adj_list = {}
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ": ", self.adj_list[vertex])
+
+    def add_vertex(self, value):
+        if value not in self.adj_list.keys():
+            self.adj_list[value] = []
+            return True
+        return False
+
+
+graph = Graph()
+
+graph.add_vertex("J")
+graph.add_vertex("O")
+
+graph.print_graph()
