60d2de34ca24a6cd5176a7d4407243e9c72edee4
joaopulsz <joaopulsz@hotmail.com>
2022-11-10 11:12:40 +0000

    added selection sort algorithm

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -8,4 +8,17 @@ def bubble_sort(my_list):
     return my_list
 
 
-print(bubble_sort([4, 6, 8, 11, 43, 1, 2, 5]))
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        minimum_index = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[minimum_index]:
+                minimum_index = j
+        if i != minimum_index:
+            item = my_list[i]
+            my_list[i] = my_list[minimum_index]
+            my_list[minimum_index] = item
+    return my_list
+
+
+print(selection_sort([4, 6, 8, 11, 43, 1, 2, 5]))
